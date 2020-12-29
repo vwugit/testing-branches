4ed11966fd3289c1d3c502cd2b459f970a1cad81
VivianWu <62616313+vwugit@users.noreply.github.com>
2020-12-29 11:53:21 -0600

    initial commit

---
+++ hello.java
@@ -0,0 +1,31 @@
+public class hello {
+    public static void main(String[] args) {
+        int numberOfPets;
+        long numberOfDays;
+        short iceCreamFlavors;
+        byte numberOfChildren = 3;
+        float dollars = 12.52f;
+        double time = 5.36;
+
+        numberOfPets = 4;
+        numberOfDays = 7;
+        iceCreamFlavors = 3;
+
+        short sum = (short) (iceCreamFlavors + numberOfChildren);
+        System.out.println(sum);
+
+        int a = 20;
+        int b = 25;
+        int c = 50;
+        int d = 10;
+
+        int product = a * d;
+        System.out.println(product);
+
+        int sum2 = b + c;
+        System.out.println(sum2);
+
+        int sum3 = c/d;
+        System.out.println(sum3);
+    }
+}
